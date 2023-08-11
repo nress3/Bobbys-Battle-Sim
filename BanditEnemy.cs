0683942e4ce46825c934826e2f975a48b183fd20
Nathan Ress <nress3@gmail.com>
2023-08-10 17:32:35 -0700

    fleshing out the bandit class

--- BanditEnemy.cs
+++ BanditEnemy.cs
@@ -14,21 +14,35 @@ namespace textGameMaybe
         {
             get { return headBanditName; }
         }
-
-
-
         protected string[] banditWeaponChoices = { "knife", "sword", "club", "ax" };
         protected string banditweapon;
         protected int banditWeaponDamage;
 
 
+        protected string[] banditNames = { "Barret Black Lung", "Gerard the Fool", "Thorburn the Hunter", "Ravi Rebel", 
+                                            "Irving Three Toes", "Roberto Grimm", "Sawyer Serpent", "Roy Smiley", "Carlos Hawkeye", 
+                                            "Steve the Serpent", "Mad Man Milton", "Cyrus Razor", "Brandon Bulletproof", "Alex the Liar", 
+                                            "Dwayne the Brute", "Reginald the Shark", "Holden the Devil", "Newton Smiley", 
+                                            "Terris Bullseye", "Hyatt Witty", "Jagger Blowhard" };
+        protected string banditName;
+
+        protected string[] banditInsults = { "Have at you then!", "Who are you?", "You dare you desecrate this place!", "You won't get past me!", "You're as good as dead!",
+                                            "I'll have your head!", "You'll never make it out of here alive.", "Your mother was a hamster and your father smelled of elderberries!",
+                                            "I'll kill you!", "Get Out!", "Get fucked bahd!", "That's it, you're dead!", "I'll mount your skull on my mantle!"};
+
+
+
+
         public BanditEnemy()
         {
+            banditName = banditNames[banditRandom.Next(banditNames.Length)];
             banditweapon = banditWeaponChoices[banditRandom.Next(banditWeaponChoices.Length)];
             banditWeaponDamage = banditRandom.Next(0, 4);
             health = banditRandom.Next(10, 16);
             toughness = banditRandom.Next(0, 4);
             strength = banditRandom.Next(0, 4);
+
+            Console.WriteLine($"There is a bandit in the room named {banditName}. He points at you and says {banditInsults[banditRandom.Next(banditInsults.Length)]}.");
         }
 
 
